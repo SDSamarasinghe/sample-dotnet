3d72c658633d0722b8a81d04f730b890f259dd13
SDSamarasinghe <it20605902@my.sliit.lk>
2024-12-17 15:41:31 +0530

    Add simple calculator functionality to ConsoleApp1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Simple Calculator!");
+
+            Console.Write("Enter the first number: ");
+            string input1 = Console.ReadLine();
+            double number1 = Convert.ToDouble(input1);
+
+            Console.Write("Enter the second number: ");
+            string input2 = Console.ReadLine();
+            double number2 = Convert.ToDouble(input2);
+
+            double result = number1 + number2;
+
+            Console.WriteLine($"The result of adding {number1} and {number2} is {result}.");
+        }
+    }
+}
